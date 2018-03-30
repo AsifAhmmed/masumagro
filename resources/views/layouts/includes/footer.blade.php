f02696c5fb01b7d20bf73c7e392d4562b2b41c56
mahfuzh <mahfuzhur.rahman@gmail.com>
2018-03-30 16:29:57 +0600

    deleted

--- resources/views/layouts/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.5.0
-    </div>
-    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="#">{{ Session::get('name') }}</a>.</strong> All rights
-    reserved.
-  </footer>
